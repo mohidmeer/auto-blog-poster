2b2e6a7c3959b7293f6f9b484602a18db236115a
Mohid Meer <mohidfauji@gmail.com>
2025-03-28 22:53:01 +0500

    Added the schedules

--- ui/src/pages/Dashboard.tsx
+++ ui/src/pages/Dashboard.tsx
@@ -1,4 +1,4 @@
-import { Coins, LogOut, Plus } from "lucide-react"
+import { Coins, Delete, Edit, LogOut, Pause, Plus, Timer, Trash, Workflow } from "lucide-react"
 import { Button } from "../components/ui/button"
 import { useNavigate } from "react-router-dom";
 import { getUsername } from "../lib/utils";
@@ -14,9 +14,8 @@ const Dashboard = () => {
     };
     return (
         <div className="p-10 ">
-
             <div className="max-w-6xl mx-auto shadow-2xl p-4 border rounded-md">
-                <div className="flex justify-between items-center">
+                <div className="flex justify-between items-center border-b pb-1">
                     <div className="flex items-center gap-1 font-bold">
                         <div className="size-6  bg-primary" />
                         Blog Flix
@@ -35,7 +34,7 @@ const Dashboard = () => {
                     </div>
 
                 </div>
-                <div className="h-[50vh] flex flex-col justify-center p-4">
+                <div className="h-[50vh] flex flex-col justify-center p-4 ">
                     <h1 className="text-6xl">Welcome back,<br />
                         <span className="font-bold">{getUsername()}</span>
                     </h1>
@@ -59,6 +58,19 @@ const Dashboard = () => {
                         </div>
                     </div>
                 </div>
+                <div className="p-4">
+                    <h4 className="text-2xl font-bold mb-4 ">Your Schedules</h4>
+
+                    <div className="grid md:grid-cols-3 lg:grid-cols-4 gap-4">
+
+                        <Schedule/>
+                        <Schedule/>
+                        <Schedule/>
+                        <Schedule/>
+        
+                    </div>
+
+                </div>
 
             </div>
 
@@ -68,3 +80,43 @@ const Dashboard = () => {
 }
 
 export default Dashboard
+
+
+
+function Schedule (){
+    return (
+        <div className="border rounded-md   cursor-pointer hover:shadow-xl transition-all duration-300">
+            <div className="p-4">
+                <Workflow/>
+            </div>
+            <div className="bg-gray-100 p-2">
+                <h5 className="text-sm font-bold text-gray-800">Schedule Post Title</h5>
+
+                <div className="flex gap-1 text-xs font-bold text-gray-500 mt-2">
+                    <Timer size={16}/>
+                    <p>daily</p>
+                    <p>|</p>
+                    <p>8:00 AM</p>
+                </div>
+                <div className="mt-2 flex items-center justify-between">
+                    <span className="bg-green-600 px-2 text-xs rounded-full font-bold text-white">
+                        active
+                    </span>
+                    <div className="flex gap-2">
+                        <button   >
+                            <Edit size={16}/>
+                        </button>
+                        <button  >
+                            <Pause size={16}/>
+                        </button>
+                        <button  >
+                            <Trash size={16} className="text-destructive"/>
+                        </button>
+
+                    </div>
+                </div>
+            
+            </div>                  
+        </div>
+    )
+}
